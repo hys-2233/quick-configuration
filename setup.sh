84160ab57bf5b8d1c1d1a67720dfe529d7a51451
韩玉森 <hys243057500@mail.ustc.edu.cn>
2024-05-26 20:42:45 +0800

    add zsh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
 sudo apt update
 
+## THE FUCK
+sudo apt update
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
+
+## ZSH
+sudo apt install zsh
+git clone https://gitee.com/kaylor/kaylor_zsh.git
+cd kaylor_zsh/
+bash ./deploy.sh
+
+
